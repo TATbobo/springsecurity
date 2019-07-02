250548aa610fadcd8e928a4d06c6888254d66803
tucker <1179468360@qq.com>
2019-07-02 20:12:13 +0800

    mavenspringboot多模块项目打包及依赖,JUnit4单元测试,伪造文件上传restful服务,记住我功能，图形验证码 6.16学习总结:   1.抽象类和接口的区别以及使用方式和适用场合   2.springsecurity短信验证码开发及原理（校验逻辑未实现）   3.泛型的简单复习 6.18:代码重构(未完成),短信验证码(未完成) 6.19:代码重构完成，短信验证码功能实现 6.20:QQ登录开发(未完成),RelaxedPropertyResolver not found BUG(未解决) 6.22:QQ登录开发(原理未完全清楚),springseucrityfilterchain加载原理(未完全清楚),spring初始化bean(未完全清楚),spring自动配置原理，遇到的bug(未总结) 6.23:QQ登录开发完成，重构完成 6.29:单机集群Session控制，使用redis 7.1:springOAuth开发app模块令牌登录

--- security-demo/src/main/java/com/tucker/securitydemo/controller/MyConnectController.java
+++ security-demo/src/main/java/com/tucker/securitydemo/controller/MyConnectController.java
@@ -6,8 +6,8 @@ import org.springframework.social.connect.web.ConnectController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/connect")
+/*@Controller
+@RequestMapping("/connect")*/
 public class MyConnectController extends ConnectController {
     public MyConnectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
         super(connectionFactoryLocator, connectionRepository);
