9ca7307a5aad7af95e1481179a58261f4200728a
tucker <1179468360@qq.com>
2019-07-03 15:25:21 +0800

    mavenspringboot多模块项目打包及依赖,JUnit4单元测试,伪造文件上传restful服务,记住我功能，图形验证码 6.16学习总结:   1.抽象类和接口的区别以及使用方式和适用场合   2.springsecurity短信验证码开发及原理（校验逻辑未实现）   3.泛型的简单复习 6.18:代码重构(未完成),短信验证码(未完成) 6.19:代码重构完成，短信验证码功能实现 6.20:QQ登录开发(未完成),RelaxedPropertyResolver not found BUG(未解决) 6.22:QQ登录开发(原理未完全清楚),springseucrityfilterchain加载原理(未完全清楚),spring初始化bean(未完全清楚),spring自动配置原理，遇到的bug(未总结) 6.23:QQ登录开发完成，重构完成 6.29:单机集群Session控制，使用redis 7.1:springOAuth开发app模块令牌登录

--- security-app/src/main/java/com/tucker/securityapp/config/TuckerResourceServerConfig.java
+++ security-app/src/main/java/com/tucker/securityapp/config/TuckerResourceServerConfig.java
@@ -44,8 +44,8 @@ public class TuckerResourceServerConfig extends ResourceServerConfigurerAdapter 
                 .successHandler(loginSuccessHandler)
                 .failureHandler(loginFailHandler);
 
-        http.apply(validateCodeSecurityConfig)
-                .and()
+        http/*.apply(validateCodeSecurityConfig)
+                .and()*/
                 .apply(smsCodeAuthenticationSecurityConfig)
                 .and()
                 .apply(springSocialConfigurer)
